4c65444bf4667c43b67f69d1d9deec29a3a5a007
ZhuWenfu <2583243657.com>
2022-01-04 13:36:16 +0800

    浏览文章列表、新建文章接口开发完成

---
+++ PersonalBlog/PersonalBlog/urls.py
@@ -0,0 +1,23 @@
+"""PersonalBlog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls')),  # 添加DRF的登录视图，以便DRF自动为可视化接口页面生成一个用户登录的入口
+    path('api/article', include('article.urls'))
+]
---
+++ PersonalBlog/article/serializers.py
@@ -0,0 +1,14 @@
+# 序列化器
+from rest_framework import serializers
+from article.models import Article
+
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'created',
+            'updated',
+        ]
