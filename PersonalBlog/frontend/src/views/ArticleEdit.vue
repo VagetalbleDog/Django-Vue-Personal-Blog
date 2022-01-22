8e3f2f948d1a6eff129aa3d7824d53e4f24fd36b
ZhuWenfu <2583243657.com>
2022-01-22 10:30:40 +0800

    更改文章接口开发完成

---
+++ PersonalBlog/frontend/src/views/ArticleEdit.vue
@@ -0,0 +1,268 @@
+<template>
+<BlogHeader/>
+  <div v-if="is_superuser==='true'&&hasLogin" id="article-create">
+    <h3>更新或删除文章</h3>
+    <form>
+      <div class="form-elem">
+        <span>标题：</span>
+        <input v-model="title" type="text" placeholder="输入文章标题">
+      </div>
+
+      <div class="form-elem">
+        <span>分类：</span>
+        <span v-for="category in categories" :key="category.id">
+          <!--样式也可以通过 :style 绑定-->
+          <button class="category-btn" :style="categoryStyle(category)" @click.prevent="chooseCategory(category)">
+            {{category.title}}
+          </button>
+        </span>
+      </div>
+
+      <div class="form-elem">
+        <span>标签：</span>
+        <input v-model="tags" type="text" placeholder="输入标签，用逗号分隔">
+      </div>
+
+      <div class="form-elem">
+        <span>正文：</span>
+        <textarea v-model="body" placeholder="输入正文，推荐使用Markdown语法，让你的文章更有层次感" rows="20" cols="80"></textarea>
+      </div>
+
+      <div class="form-elem" style="padding-top:10px;text-align: center">
+        <button v-on:click.prevent="submit" style="width: auto;">提交修改</button>
+      </div>
+      <div class="form-elem" style="padding-top:10px;text-align: center">
+        <button v-on:click.prevent="showingDeleteAlert = true" class="delete-btn" style="width: auto">删除文章</button>
+        <div :class="{shake:showingDeleteAlert}">
+          <button v-if="showingDeleteAlert" class="confirm-btn" @click.prevent="delete_article(article_id)" style="width: auto">您确定吗？</button>
+        </div>
+      </div>
+    </form>
+</div>
+  <div v-else-if="hasLogin" style="padding-top: 150px;font-size: x-large">
+    <p style="text-align: center">抱歉，您的权限暂不支持！请<router-link :to="{name:'RequireAuthorization'}" class="login-link">联系开发者获取权限</router-link>后再进行相关操作.</p>
+    <p style="text-align: center"><router-link to="{name:'Home'}">您也可以点击这里回到主页</router-link></p>
+  </div>
+  <div v-else style="padding-top: 150px;font-size: x-large">
+    <p style="text-align: center">抱歉，您还未登录！请<router-link to="/login" class="login-link">登录</router-link>后再进行相关操作.</p>
+    <p style="text-align: center"><router-link to="{name:'Home'}">您可以点击这里回到主页</router-link></p>
+  </div>
+<BlogFooter/>
+</template>
+
+<script>
+import BlogHeader from "@/components/BlogHeader";
+import BlogFooter from "@/components/BlogFooter";
+import axios from "axios";
+import authorization from "@/utils/authorization";
+export default {
+  name: "ArticleEdit",
+  components: {BlogFooter, BlogHeader},
+  data:function (){
+    return{
+      title:'',
+      body:'',
+      categories:[],
+      selectedCategory:null,
+      tags:'',
+      showingDeleteAlert:false,
+      article_id: this.$route.params.article_id
+    }
+  },
+  computed:{
+    is_superuser(){
+      return localStorage.getItem('is_superuser.myblog')
+    },
+    hasLogin(){
+      return (localStorage.getItem('login.myblog') === "1")
+    },
+  },
+  mounted() {
+      // 页面初始化时获取所有分类
+      axios
+        .get('/api/category/')
+        .then(response => this.categories = response.data);
+      // 获取旧数据
+      const that = this;
+      axios
+        .get('/api/article/' + this.$route.params.article_id + '/')
+        .then(function (response) {
+          const data = response.data;
+          that.title = data.title;
+          that.body = data.body;
+          that.selectedCategory = data.category;
+          that.tags = data.tags.join(',');
+          that.articleID = data.id;
+        })
+    },
+  methods:{
+    // 根据分类是否被选中，按钮的颜色发生变化
+    // 这里可以看出Css也是可以被vue所绑定的，肥肠的方便
+    categoryStyle(category){
+      if(this.selectedCategory!==null && category.id===this.selectedCategory.id){
+        return{
+          backgroundColor:'green',
+        }
+      }
+      return {
+        backgroundColor: 'lightgrey',
+        color:'black',
+      }
+    },
+    // 选取分类的方法
+    chooseCategory(category){
+      // 如果点击已选取的分类。则将selectedCategory置空
+      if(this.selectedCategory!==null && category.id===this.selectedCategory.id){
+        this.selectedCategory = null;
+      }
+      // 若点击没有被选中的，则选中他
+      else {
+        this.selectedCategory = category;
+      }
+    },
+    //删除文章方法
+    delete_article(article_id){
+            //获取token
+            const that = this;
+            const token = localStorage.getItem('access.myblog');
+            authorization()
+              .then(function (response){
+                if(response[0]){
+                  const url = '/api/article/'+article_id;
+                  console.log('删除文章进程开始，文章id：',article_id);
+                  axios
+                    .delete(url,{
+                      headers:{Authorization: 'Bearer '+token}
+                    })
+                    .then(function (){
+                      console.log('文章已删除.');
+                      alert('文章已删除！');
+                      const username = localStorage.getItem('username.myblog')
+                      that.$router.push({name:'MyArticle',params:{author_name: username}})
+                    })
+                    .catch(function (error){
+                      console.log(error.message);
+                      alert("似乎出了点问题。")
+                    })
+                }
+                else {
+                  alert('令牌过期，请重新登录！')
+                }
+              })
+          },
+    // 提交方法
+    submit(){
+      const that = this;
+      // 验证一下权限
+      authorization()
+        .then(function (response){
+          if (response[0]){
+            let data = {
+              title:that.title,
+              body:that.body,
+            };
+            // 添加分类
+            if(that.selectedCategory){
+              data.category_id = that.selectedCategory.id;
+            }
+            // 标签预处理
+            data.tags = that.tags
+                // 逗号分割标签
+              .split(/[,.，]/)
+                // 剔除标签首位空格
+              .map(x=>x.trim())
+                // 剔除长度为0的无效标签
+              .filter(x=>x.charAt(0)!=='');
+            // 获取token
+            const token = localStorage.getItem('access.myblog');
+            const url = '/api/article/'+that.article_id+'/';
+            // 发送请求POST
+            axios
+              .put(url,data,{
+                headers:{Authorization: 'Bearer '+token}
+              })
+              .then(function (response){
+                console.log("文章修改成功!文章id：",that.article_id);
+                alert("文章修改成功！");
+                that.$router.push({name:'ArticleDetail',params:{id:response.data.id}});
+              })
+              .catch(function (error){
+                console.log(error.message);
+                alert('似乎除了点问题，您可以联系开发者解决.');
+              })
+          }else {
+            alert("登录已过期，请重新登录哦")
+            that.$router.push({name:'Login'})
+          }
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .delete-btn {
+        background-color: darkred;
+        margin-bottom: 10px;
+    }
+  .category-btn {
+    margin-right: 10px;
+    font-size: 13px;
+  }
+  #article-create {
+    text-align: center;
+    font-size: large;
+  }
+  form {
+    text-align: left;
+    padding-left: 100px;
+    padding-right: 10px;
+  }
+  .form-elem {
+    padding: 10px;
+  }
+  input {
+    height: 25px;
+    padding-left: 10px;
+    width: 50%;
+  }
+  button {
+    height: 35px;
+    cursor: pointer;
+    border: none;
+    outline: none;
+    background: steelblue;
+    color: whitesmoke;
+    border-radius: 5px;
+    width: 60px;
+  }
+  .confirm-btn {
+        width: 80px;
+        background-color: darkorange;
+    }
+  .shake {
+        animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+        transform: translate3d(0, 0, 0);
+        backface-visibility: hidden;
+        perspective: 1000px;
+    }
+    @keyframes shake {
+        10%,
+        90% {
+            transform: translate3d(-1px, 0, 0);
+        }
+        20%,
+        80% {
+            transform: translate3d(2px, 0, 0);
+        }
+        30%,
+        50%,
+        70% {
+            transform: translate3d(-4px, 0, 0);
+        }
+        40%,
+        60% {
+            transform: translate3d(4px, 0, 0);
+        }
+    }
+</style>
---
+++ PersonalBlog/frontend/src/views/MyArticle.vue
@@ -0,0 +1,206 @@
+<template>
+<BlogHeader/>
+<div v-if="is_superuser==='true'&&hasLogin">
+  <p style="text-align: left;padding-top: 70px;font-size: x-large">{{author_name}}&nbsp;您好！&nbsp;您创作的文章共{{info.count}}篇，如下：</p>
+    <div class="row mt-2" v-for="article in info.results" v-bind:key="article.title">
+        <!-- 文章内容 -->
+        <div class="col-12">
+                <router-link v-if="article.category !== null" class="category" :to="{name:'CategoryDetail',params: {category_name:article.category.title}}">{{article.category.title}}</router-link>
+            <!-- 标签 -->
+                <span v-for="tag in article.tags" v-bind:key="tag" class="tag">
+                  {{ tag }}
+                </span>
+            <!-- 标题 -->
+            <h3>
+                <b>
+                    <router-link :to="{ name:'ArticleDetail',params: { id: article.id }}" class="article-title">{{ article.title }}</router-link>
+                </b>
+            </h3>
+          <router-link :to="{name:'ArticleEdit',params:{article_id:article.id}}" class="category" style="background-color:lightslategrey;float: right;">编辑此文章</router-link>
+            <!-- 摘要 -->
+            <!-- 注脚 -->
+            <p>
+                <!-- 附加信息 -->
+                <span style="color: blue;">
+                    {{ formatted_time(article.created) }} 发布&nbsp;&nbsp;&nbsp;
+                </span>
+                <span style="color: darkred;">
+                    {{ formatted_time(article.updated) }} 更新
+                </span>
+            </p>
+            <hr>
+        </div>
+</div>
+    <div id="paginator" style="padding-top: 50px">
+      <span v-if="is_page_exists('previous')">
+        <router-link :to="get_path('previous')" style="color: #42b983">
+          上一页
+        </router-link>
+      </span>
+      <span  class="current-page" v-if="get_page_param('current')" style="color: blue">
+        第{{ get_page_param('current') }}页
+      </span>
+      <span  class="current-page" v-if="!get_page_param('current')" style="color: blue">
+        第1页
+      </span>
+      <span v-if="is_page_exists('next')">
+        <router-link :to="get_path('next')" style="color: #42b983">
+          下一页
+        </router-link>
+      </span>
+    </div>
+  </div>
+  <div v-else-if="hasLogin" style="padding-top: 150px;font-size: x-large">
+    <p style="text-align: center">抱歉，您的权限暂不支持！请<router-link :to="{name:'RequireAuthorization'}" class="login-link">联系开发者获取权限</router-link>后再进行相关操作.</p>
+    <p style="text-align: center"><router-link to="{name:'Home'}">您也可以点击这里回到主页</router-link></p>
+  </div>
+  <div v-else style="padding-top: 150px;font-size: x-large">
+    <p style="text-align: center">抱歉，您还未登录！请<router-link to="/login" class="login-link">登录</router-link>后再进行相关操作.</p>
+    <p style="text-align: center"><router-link to="{name:'Home'}">您可以点击这里回到主页</router-link></p>
+  </div>
+<BlogFooter/>
+</template>
+
+<script>
+    import axios from 'axios';
+    import BlogHeader from "@/components/BlogHeader";
+    import BlogFooter from "@/components/BlogFooter";
+
+
+    export default {
+        name: 'App',
+      components: {BlogFooter, BlogHeader},
+      data: function () {
+            return {
+                info:'',
+              author_name: this.$route.params.author_name,
+            }
+        },
+        computed: {
+          is_superuser() {
+            return localStorage.getItem('is_superuser.myblog')
+          },
+          hasLogin() {
+            return (localStorage.getItem('login.myblog') === "1")
+          },
+        },
+        mounted() {
+            this.get_article_data()
+        },
+        methods:{
+          formatted_time:function (iso_date_string){
+            const date = new Date(iso_date_string);
+            return date.toLocaleDateString()
+          },
+          // 判断页面是否存在
+          is_page_exists:function (direction){
+            if (direction === 'next'){
+              return this.info.next !== null
+            }
+            return this.info.previous !== null
+          },
+          get_page_param:function (direction) {
+            try {
+              let url_string;
+              switch (direction) {
+                case 'next':
+                  url_string = this.info.next;
+                  break;
+                case 'previous':
+                  url_string = this.info.previous;
+                  break;
+                default:
+                  return this.$route.query.page
+              }
+              const url = new URL(url_string);
+              return url.searchParams.get('page')
+            } catch (err) {
+              console.log(err.message);
+            }
+          },
+          get_article_data:function (){
+            let url = '/api/article';
+            let params = new URLSearchParams();
+            params.append_if_exists('page', this.$route.query.page);
+            const paramsString = params.toString();
+            url += '/?'+'&search='+this.author_name+'&'+paramsString
+            axios
+                .get(url)
+                .then(response => (this.info = response.data))
+                .catch(function (error){
+                  console.log(error.message);
+                  if(error.response.status===404){
+                    alert('您似乎还没有写文章哦')
+                  }
+                })
+          },
+          get_path:function (direction){
+            let url = '';
+            try{
+              switch (direction){
+                case 'next':
+                  if (this.info.next !== undefined){
+                    url += (new URL(this.info.next)).search
+                  }
+                  break;
+                case 'previous':
+                  if (this.info.previous!== undefined){
+                    url += (new URL(this.info.previous)).search
+                  }
+                  break;
+              }
+            }
+            catch {
+              return url
+            }
+            return url
+          }
+        },
+        watch:{
+          $route(){
+            this.get_article_data()
+          }
+        }
+    }
+</script>
+
+<style scoped>
+    .category{
+      padding: 5px 10px 5px 10px;
+      margin: 5px 5px 5px 0;
+      font-family: Georgia, Arial , sans-serif;
+      font-size: small;
+      background-color: darkgreen;
+      color: whitesmoke;
+      border-radius: 15px;
+      text-decoration: none;
+    }
+    .article-title {
+        font-size: large;
+        font-weight: bolder;
+        color: black;
+        text-decoration: none;
+        padding: 5px 0 5px 0;
+    }
+    .tag{
+      padding: 2px 5px 2px 5px;
+      margin: 5px 5px 5px 0;
+      font-family: Georgia, Arial ,sans-serif;
+      font-size:small;
+      background-color: dodgerblue;
+      color: whitesmoke;
+      text-shadow: white;
+      border-radius: 5px;
+    }
+    #paginator{
+      text-align: center;
+      padding-top: 50px;
+    }
+    a{
+      color: black;
+    }
+    .current-page{
+      font-size: x-large;
+      font-weight: bold;
+    }
+</style>
