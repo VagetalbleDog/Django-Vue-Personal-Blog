a82d6511e40c7521d0f821987f2a9cc7d04475a3
ZhuWenfu <2583243657.com>
2022-02-08 19:28:03 +0800

    网站底部添加备案号

--- PersonalBlog/frontend/src/components/BlogFooter.vue
+++ PersonalBlog/frontend/src/components/BlogFooter.vue
@@ -5,7 +5,7 @@
 
 <footer class="py-3 bg-dark fixed-bottom">
     <div class="container">
-        <p class="m-0 text-center text-white">CopyRight © 2022 ZhuWenfu's Personal Blog.&nbsp;&nbsp;All rights reserved.</p>
+        <p class="m-0 text-center text-white">&nbsp;<a HREF="https://beian.miit.gov.cn/">辽ICP备2022001348号</a>&nbsp;CopyRight © 2022 ZhuWenfu's Personal Blog.&nbsp;&nbsp;All rights reserved.</p>
     </div>
 </footer>
 </template>
