d2ea31e2cf7f327d8d8e9b457df79216057f31f6
VerzWeiFelt <95014669+VagetalbleDog@users.noreply.github.com>
2022-07-02 13:45:56 +0800

    Update settings.py

--- PersonalBlog/PersonalBlog/settings.py
+++ PersonalBlog/PersonalBlog/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend/dist/")
+    os.path.join(BASE_DIR, "frontend/build/")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
